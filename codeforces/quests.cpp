31ff95eb3d56e5d5dbd717f16a994aadcd449d95
rahulsiloniya <rsiloniya@gmail.com>
2023-12-24 22:36:30 +0530

    quests.cpp

--- codeforces/quests.cpp
+++ codeforces/quests.cpp
@@ -6,6 +6,21 @@ int main() {
     int test;
     cin >> test;
     while (test--) {
-        
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        vector<int> barr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        for (int i = 0; i < n; i++) cin >> barr[i];
+
+        int res = 0;
+        int sum = 0;
+        int repmax = 0;
+        for (int i = 1; i <= min(n, k); i++) {
+            sum += arr[i - 1];
+            repmax = max(repmax, barr[i - 1]);
+            res = max(res, sum + (k - i) * repmax);
+        }
+        cout << res << endl;
     }
 }
